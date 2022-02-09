ccf3d954af7fb3da6e310a25f0b855506dc540f9
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2022-02-09 09:02:32 +0530

    - popover UI bug resolved - Additional dummy UI content is added to settings page.

--- src/app/shared/components/popover-controller/popover-dialogue.component.ts
+++ src/app/shared/components/popover-controller/popover-dialogue.component.ts
@@ -6,9 +6,9 @@ import { LogoutComponent } from '../logout/logout.component';
   selector: 'app-popover-dialogue',
   template: `
     <ion-list>
-      <ion-item [routerLink]="['/tabs/settings']" button (click)="close()">Settings</ion-item>
-      <ion-item  detail="false" button (click)="goGit(); close()">GitHub Repo</ion-item>
-      <ion-item  lines="none" detail="false" button (click)="logout(); close()">Logout</ion-item>
+      <ion-item detail="false" [routerLink]="['/tabs/settings']" button (click)="close()">Settings</ion-item>
+      <ion-item detail="false"                                   button (click)="goGit(); close()">GitHub Repo</ion-item>
+      <ion-item detail="false" lines="none"                      button (click)="logout(); close()">Logout</ion-item>
     </ion-list>
   `
 })
